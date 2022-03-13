88197113c5b2ba2a0e95e6e01a5bc6999dcb5de5
JonasBM <jonas.mela@gmail.com>
2022-03-13 21:01:45 +0100

    REFACTOR: move app files

---
+++ src/app/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className='rounded-xl shadow-xl p-5 w-64 bg-gradient-to-br from-indigo-500'>
+          TailwindCSS test
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
