c6a6cb9fa2ecbf1710d52479c6b95b2cc436b371
JonasBM <jonas.mela@gmail.com>
2022-03-14 10:46:42 +0100

    CHORE: add admin login action

--- src/store/actions/sessionActions.js
+++ src/store/actions/sessionActions.js
@@ -1,10 +1,16 @@
 export const ACTION_SESSION_LOGIN_USER_SET = '[userLoginSession] SET'
+export const ACTION_SESSION_LOGIN_ADMIN_SET = '[adminLoginSession] SET'
 export const ACTION_SESSION_LOGOUT_SET = '[logoutSession] SET'
 
 
 export const sessionLoginUserSetAction = user => ({
     type: ACTION_SESSION_LOGIN_USER_SET,
     payload: user
+})
+
+export const sessionLoginAdminSetAction = admin => ({
+    type: ACTION_SESSION_LOGIN_ADMIN_SET,
+    payload: admin
 })
 
 export const sessionLogoutSetAction = () => ({
