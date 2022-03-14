1bb55ab86753ac6e1547621be265a021e144b784
JonasBM <jonas.mela@gmail.com>
2022-03-14 08:22:19 +0100

    CHORE: create header file

---
+++ src/app/header/Header.jsx
@@ -0,0 +1,16 @@
+import { NavLink} from 'react-router-dom'
+
+
+const Header = () => {
+    
+    return (
+        <>
+            <header className="app-header">
+                <h1>Human vs. Zombies</h1>
+            </header>
+        </>
+    )
+}
+
+
+export default Header
