0bc49f96a59084cf5d99bbda7f539c3a5c54ed3b
JonasBM <jonas.mela@gmail.com>
2022-03-14 08:41:03 +0100

    FEAT: add navigation links

--- src/app/header/Header.jsx
+++ src/app/header/Header.jsx
@@ -7,6 +7,16 @@ const Header = () => {
         <>
             <header className="app-header">
                 <h1>Human vs. Zombies</h1>
+                <nav>
+                    <ul>
+                        <li>
+                            <NavLink className="app-link" to="/">Home</NavLink>
+                        </li>
+                        <li>
+                            <NavLink className="app-link" to="/game">Game</NavLink>
+                        </li>
+                    </ul>
+                </nav>
                 <div className='rounded-xl shadow-xl p-5 bg-gradient-to-br from-indigo-500'>
                     TailwindCSS test (Header above ^)
                 </div>
