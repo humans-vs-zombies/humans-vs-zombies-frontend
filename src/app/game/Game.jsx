ef75e5dc2562618ca53f15172bcc09dd3b18a6bc
JonasBM <jonas.mela@gmail.com>
2022-03-14 13:45:58 +0100

    FEAT: navigation guard for game-page

--- src/app/game/Game.jsx
+++ src/app/game/Game.jsx
@@ -1,4 +1,13 @@
+import { useSelector } from "react-redux"
+import { Navigate } from "react-router-dom"
+
 const Game = () => {
+
+    const { loggedIn } = useSelector(state => state.sessionReducer)
+    if (!loggedIn) {
+        return <Navigate to="/" />
+    }
+
     return (
         <>
             <h1>Game page</h1>
