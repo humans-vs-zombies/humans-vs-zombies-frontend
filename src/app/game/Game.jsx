b2f913f6dabcc12203c8f4c89ad5ce3a36f3be44
Jorgsaa <jorgen.saanum@gmail.com>
2022-03-14 15:18:57 +0100

    STYLE: added page content to Game page component

--- src/app/game/Game.jsx
+++ src/app/game/Game.jsx
@@ -1,17 +1,39 @@
-import { useSelector } from "react-redux"
-import { Navigate } from "react-router-dom"
+import { useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
+import { SquadList } from "./SquadList";
 
 const Game = () => {
+  const { loggedIn } = useSelector((state) => state.sessionReducer);
 
-    const { loggedIn } = useSelector(state => state.sessionReducer)
+  return (
+    <>
+      {!loggedIn && <Navigate to="/" />}
+      <div className="h-full flex flex-col bg-orange-400">
+        <div className="flex flex-grow bg-gray-300 md:pt-2 p-0 px-4">
+          <button>Join</button>
+          <div className="flex-grow text-center">
+            <div className="font-bold">Game Name</div>
+            <div className="font-thin">Game description</div>
+          </div>
+          <button>Rules</button>
+        </div>
 
-    return (
-        <>
-            { !loggedIn && <Navigate to="/" /> }
-            <h1>Game page</h1>
-        </>
-    )
-}
+        <div className="h-full flex flex-grow bg-red-600 h-100">
+          <div className="flex-grow text-center bg-blue-600">
+            <h1 className="font-semibold">Squads</h1>
+            <SquadList
+              squads={[
+                { name: "Squad 1" },
+                { name: "Squad 2" },
+                { name: "Squad 3" },
+              ]}
+            />
+          </div>
+          <div className="flex-grow-1 aspect-square bg-green-600 text-center align">Map</div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-
-export default Game
+export default Game;
