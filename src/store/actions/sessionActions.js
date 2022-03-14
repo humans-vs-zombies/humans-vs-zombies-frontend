debfe3e037b1b0b75a2f3fa8fad2c0119205b572
JonasBM <jonas.mela@gmail.com>
2022-03-14 10:31:06 +0100

    CHORE: add logout session action

--- src/store/actions/sessionActions.js
+++ src/store/actions/sessionActions.js
@@ -1,7 +1,12 @@
 export const ACTION_SESSION_LOGIN_USER_SET = '[userLoginSession] SET'
+export const ACTION_SESSION_LOGOUT_SET = '[logoutSession] SET'
 
 
 export const sessionLoginUserSetAction = user => ({
     type: ACTION_SESSION_LOGIN_USER_SET,
     payload: user
+})
+
+export const sessionLogoutSetAction = () => ({
+    type: ACTION_SESSION_LOGOUT_SET
 })
