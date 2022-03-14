47b959a96ad836a7027a7942e22633050232208c
JonasBM <jonas.mela@gmail.com>
2022-03-14 08:36:08 +0100

    FEAT: add header to app

--- src/app/header/Header.jsx
+++ src/app/header/Header.jsx
@@ -7,6 +7,9 @@ const Header = () => {
         <>
             <header className="app-header">
                 <h1>Human vs. Zombies</h1>
+                <div className='rounded-xl shadow-xl p-5 bg-gradient-to-br from-indigo-500'>
+                    TailwindCSS test (Header above ^)
+                </div>
             </header>
         </>
     )
