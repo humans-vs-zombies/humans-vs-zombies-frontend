a6c027a42624298f9870338766d590f086cb91ee
Jorgsaa <jorgen.saanum@gmail.com>
2022-03-14 12:43:50 +0100

    FIX: now includes index.html in TailwindCSS content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
+    content: [
+      "./src/app/**/*.{html,js,jsx}",
+      "./public/index.html"
+    ],
     theme: {
       extend: {},
     },
