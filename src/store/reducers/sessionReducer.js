29d9b018cbe85d89bacc915156d791e61ea621c2
JonasBM <jonas.mela@gmail.com>
2022-03-14 09:42:48 +0100

    FEAT: add reducer for user login session

--- src/store/reducers/sessionReducer.js
+++ src/store/reducers/sessionReducer.js
@@ -1,6 +1,24 @@
+import { ACTION_SESSION_LOGIN_USER_SET } from "../actions/sessionActions"
+
 
 const initialState = {
     user: "",
     loggedIn: false,
     userType: ""
+}
+
+
+export const sessionReducer = (state = initialState, action) => {
+
+    switch (action.type) {
+        case ACTION_SESSION_LOGIN_USER_SET:
+            return {
+                ...state,
+                user: action.payload,
+                loggedIn: true,
+                userType: "user"
+            }
+        default:
+            return state
+    }
 }
