067791098aebd220de59f7e53dea05d8bf812164
Jorgsaa <jorgen.saanum@gmail.com>
2022-03-15 11:04:39 +0100

    FEAT: added support for showing number of humans alive

--- src/app/game/SquadListElement.jsx
+++ src/app/game/SquadListElement.jsx
@@ -5,7 +5,8 @@ export const SquadListElement = ({ squad }) => {
     <div className="p-1">
       <div className="flex bg-gray-400 text-center items-center">
         <button className="whitespace-nowrap p-2">{squad.name}</button>
-        <div className="md:block hidden">4/10</div>
+        <div className="sm:block hidden">{ squad.players.filter(p => p.isAlive).length + "/" + squad.players.length}</div>
+        <span className="md:block hidden pl-1">Alive</span>
         <button className="w-16">Join</button>
       </div>
     </div>
