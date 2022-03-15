af2436cbb678dac7485a4395942743e2583250d6
JonasBM <jonas.mela@gmail.com>
2022-03-15 12:52:55 +0100

    REFACTOR: localhost adress

--- src/api/GameAPI.js
+++ src/api/GameAPI.js
@@ -4,6 +4,7 @@ import { handleFirstResponse } from "../utils/apiUtils"
 export const GamesAPI = {
     getGames() {
         return fetch("https://humans-vs-zombies-backend.herokuapp.com/api/v1/game")
+        //return fetch("http://localhost:8080/api/v1/game")
         .then(handleFirstResponse)
     }
 }
