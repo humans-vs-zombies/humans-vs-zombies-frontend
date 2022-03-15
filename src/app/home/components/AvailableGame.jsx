0b2206a3a6b4eea02c1ca9f74831fe1dfa88c405
JonasBM <jonas.mela@gmail.com>
2022-03-15 20:52:50 +0100

    FEAT: set current game in session and state

--- src/app/home/components/AvailableGame.jsx
+++ src/app/home/components/AvailableGame.jsx
@@ -1,15 +1,25 @@
+import { useDispatch } from "react-redux";
+import { sessionCurrentGameSetAction } from "../../../store/actions/sessionActions";
+
 const AvailableGame = ({ index, game }) => {
 
+    const dispatch = useDispatch()
+
+    // Tailwind class-variables
     let rowBg = index % 2 === 0 ? "" : "bg-gray-100"
+
+    const handleGameBtnClick = event => {
+        dispatch(sessionCurrentGameSetAction(game.id))
+    }
 
     return (
         <>
             <tbody>
                 <tr className={`border ${rowBg} hover:bg-gray-200 cursor-pointer`}>
-                    <td className="py-2 px-6 text-s text-left">{ game.title }</td>
-                    <td className="py-2 px-6 text-s text-left">{ game.date }</td>
-                    <td className="py-2 px-6 text-s text-left">{ game.participants}</td>
-                    <td className="py-2 px-6 text-s text-left">{ game.state }</td>
+                    <td className="py-2 px-6 text-s text-left" onClick={ handleGameBtnClick }>{ game.title }</td>
+                    <td className="py-2 px-6 text-s text-left" onClick={ handleGameBtnClick }>{ game.date }</td>
+                    <td className="py-2 px-6 text-s text-left" onClick={ handleGameBtnClick }>{ game.participants}</td>
+                    <td className="py-2 px-6 text-s text-left" onClick={ handleGameBtnClick }>{ game.state }</td>
                 </tr>
             </tbody>
         </>
