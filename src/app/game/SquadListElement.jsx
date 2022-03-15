f41beddb7aed670692185c01abbec2e6d4881785
Jorgsaa <jorgen.saanum@gmail.com>
2022-03-15 10:41:11 +0100

    FEAT: added more content to SquadListElement

--- src/app/game/SquadListElement.jsx
+++ src/app/game/SquadListElement.jsx
@@ -2,8 +2,12 @@ import React from "react";
 
 export const SquadListElement = ({ squad }) => {
   return (
-    <div>
+    <div className="p-1">
+      <div className="flex bg-gray-400 text-center items-center">
         <button className="whitespace-nowrap p-2">{squad.name}</button>
+        <div className="md:block hidden">4/10</div>
+        <button className="w-16">Join</button>
+      </div>
     </div>
-  )
+  );
 };
