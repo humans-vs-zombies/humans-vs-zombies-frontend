388912f5a44cbbfe1b8f89888dd02bef1ffc71f5
JonasBM <jonas.mela@gmail.com>
2022-03-15 20:42:47 +0100

    CHORE: set current game middleware

--- src/store/middleware/sessionMiddlware.js
+++ src/store/middleware/sessionMiddlware.js
@@ -1,4 +1,4 @@
-import { ACTION_SESSION_INIT, ACTION_SESSION_LOGIN_ADMIN_SET, ACTION_SESSION_LOGIN_USER_SET, ACTION_SESSION_LOGOUT_SET, sessionLoginAdminSetAction, sessionLoginUserSetAction } from "../actions/sessionActions"
+import { ACTION_SESSION_CURRENT_GAME_SET, ACTION_SESSION_INIT, ACTION_SESSION_LOGIN_ADMIN_SET, ACTION_SESSION_LOGIN_USER_SET, ACTION_SESSION_LOGOUT_SET, sessionLoginAdminSetAction, sessionLoginUserSetAction } from "../actions/sessionActions"
 
 
 export const sessionMiddleware = ({ dispatch }) => next => action => {
@@ -6,6 +6,7 @@ export const sessionMiddleware = ({ dispatch }) => next => action => {
     next(action)
 
     const userSessionKey = "user-ls"
+    const currentGameSessionKey = "currentGame-ls"
 
     switch (action.type) {
         case ACTION_SESSION_INIT:
@@ -27,8 +28,11 @@ export const sessionMiddleware = ({ dispatch }) => next => action => {
             localStorage.setItem(userSessionKey, JSON.stringify(action.payload))
             break
         case ACTION_SESSION_LOGOUT_SET:
+            localStorage.removeItem(currentGameSessionKey)
             localStorage.removeItem(userSessionKey)
             break
+        case ACTION_SESSION_CURRENT_GAME_SET:
+            localStorage.setItem(currentGameSessionKey, JSON.stringify(action.payload))
         default:
             break
     }
