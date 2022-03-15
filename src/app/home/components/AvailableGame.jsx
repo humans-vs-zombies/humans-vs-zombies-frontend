1d5299c92796d73dfd3386838bea85b7b459cc27
JonasBM <jonas.mela@gmail.com>
2022-03-15 21:06:53 +0100

    FEAT: remover hover for guest-user

--- src/app/home/components/AvailableGame.jsx
+++ src/app/home/components/AvailableGame.jsx
@@ -1,12 +1,14 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { sessionCurrentGameSetAction } from "../../../store/actions/sessionActions";
 
 const AvailableGame = ({ index, game }) => {
 
     const dispatch = useDispatch()
+    const { loggedIn } = useSelector(state => state.sessionReducer)
 
     // Tailwind class-variables
-    let rowBg = index % 2 === 0 ? "" : "bg-gray-100"
+    let rowBg = index % 2 === 0 ? "bg-white" : "bg-gray-100"
+    let rowHover = loggedIn ? "hover:bg-gray-200 cursor-pointer" : ""
 
     const handleGameBtnClick = event => {
         dispatch(sessionCurrentGameSetAction(game.id))
@@ -15,7 +17,7 @@ const AvailableGame = ({ index, game }) => {
     return (
         <>
             <tbody>
-                <tr className={`border ${rowBg} hover:bg-gray-200 cursor-pointer`}>
+                <tr className={`border ${rowBg} ${rowHover}`}>
                     <td className="py-2 px-6 text-s text-left" onClick={ handleGameBtnClick }>{ game.title }</td>
                     <td className="py-2 px-6 text-s text-left" onClick={ handleGameBtnClick }>{ game.date }</td>
                     <td className="py-2 px-6 text-s text-left" onClick={ handleGameBtnClick }>{ game.participants}</td>
