5c854271f278103a47902ab9204d03eddf5220e9
JonasBM <jonas.mela@gmail.com>
2022-03-16 13:11:32 +0100

    FEAT: navigate to edit game page

--- src/app/home/components/hoc/tdEditGamesTable.jsx
+++ src/app/home/components/hoc/tdEditGamesTable.jsx
@@ -1,6 +1,20 @@
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
 const TdEditGamesTable = ({ children }) => {
+
+    const { loggedIn, userType } = useSelector(state => state.sessionReducer)
+    const navigate = useNavigate();
+
+    const handleEditGameClick = event => {
+        if (loggedIn && userType === "admin") {
+            navigate("/game/edit")
+        }
+    }
+
+
     return (
-        <td className="py-3 px-6 text-s text-left text-blue-700 hover:underline">{ children }</td>
+        <td className="py-3 px-6 text-s text-left text-blue-700 hover:underline" onClick={ handleEditGameClick }>{ children }</td>
     )
 }
 
