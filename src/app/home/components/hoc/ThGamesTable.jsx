de191425c907669d3185f68d64cd590b49933a61
JonasBM <jonas.mela@gmail.com>
2022-03-16 09:28:22 +0100

    REFACTOR: file name

--- src/app/home/components/hoc/ThGamesTable.jsx
+++ src/app/home/components/hoc/ThGamesTable.jsx
@@ -1,8 +1,8 @@
-const HeadAppContainer = ({ children }) => {
+const ThGamesTable = ({ children }) => {
     return (
         <th className="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase">{ children }</th>
     )
 }
 
 
-export default HeadAppContainer
+export default ThGamesTable
