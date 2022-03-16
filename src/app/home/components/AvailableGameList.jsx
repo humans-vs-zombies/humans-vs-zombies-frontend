3f93d5cbd9b8392189f6550bda0d8760752c019f
JonasBM <jonas.mela@gmail.com>
2022-03-16 11:47:19 +0100

    FEAT: edit th for admin

--- src/app/home/components/AvailableGameList.jsx
+++ src/app/home/components/AvailableGameList.jsx
@@ -7,6 +7,7 @@ import ThGamesTable from "./hoc/ThGamesTable"
 const AvailableGameList = () => {
 
     const dispatch = useDispatch()
+    const { loggedIn, userType } = useSelector(state => state.sessionReducer)
     const { gamesGetAttempting, gamesGetSuccess, gamesGetError } = useSelector(state => state.gameReducer)
 
     const [ localState, setLocalState ] = useState
@@ -93,6 +94,9 @@ const AvailableGameList = () => {
                             <ThGamesTable>Date</ThGamesTable>
                             <ThGamesTable>Participants</ThGamesTable>
                             <ThGamesTable>State</ThGamesTable>
+                            { loggedIn && userType === "admin" &&
+                                <ThGamesTable>Edit</ThGamesTable>
+                            }
                         </tr>
                     </thead>
                     <TbodyGamesTable />
