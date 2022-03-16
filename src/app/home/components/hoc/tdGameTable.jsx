2aa19e26d0babb03ded3f9ea9558d7cc9cb61857
JonasBM <jonas.mela@gmail.com>
2022-03-16 13:01:54 +0100

    FEAT: navigate to selected game

--- src/app/home/components/hoc/tdGameTable.jsx
+++ src/app/home/components/hoc/tdGameTable.jsx
@@ -1,6 +1,19 @@
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom"
+
 const TdGamesTable = ({ children }) => {
+
+    const { loggedIn } = useSelector(state => state.sessionReducer)
+    const navigate = useNavigate();
+
+    const handleGameClick = event => {
+        if (loggedIn) {
+            navigate("/game")   
+        }
+    }
+
     return (
-        <td className="py-3 px-6 text-s text-left">{ children }</td>
+        <td className="py-3 px-6 text-s text-left" onClick={ handleGameClick }>{ children }</td>
     )
 }
 
