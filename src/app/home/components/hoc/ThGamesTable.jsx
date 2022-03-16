4ee8335926b6a3d0b137b2bcca07ea30aa856bf5
JonasBM <jonas.mela@gmail.com>
2022-03-16 09:27:29 +0100

    CHORE: create hoc for th (games table)

---
+++ src/app/home/components/hoc/ThGamesTable.jsx
@@ -0,0 +1,8 @@
+const HeadAppContainer = ({ children }) => {
+    return (
+        <th className="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase">{ children }</th>
+    )
+}
+
+
+export default HeadAppContainer
