00005cda5a1ed6bf3dd4a38bae16beb0a142aadb
JonasBM <jonas.mela@gmail.com>
2022-03-16 13:17:01 +0100

    FEAT: navigation guard for edit game page

--- src/app/editGame/EditGame.jsx
+++ src/app/editGame/EditGame.jsx
@@ -1,7 +1,15 @@
+import { useSelector } from "react-redux"
+import { Navigate } from "react-router-dom"
+
+
 const EditGame = () => {
+
+    const { loggedIn, userType } = useSelector(state => state.sessionReducer)
+
 
     return (
         <>
+            { (!loggedIn || userType !== "admin") && <Navigate to="/" /> }
             <h1>Edit game Page</h1>
         </>
     )
