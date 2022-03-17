8c455939d6b67627da6c7666d328b21d60b32ca4
Jonas Kristoffersen <jo_nas@live.no>
2022-03-17 22:42:38 +0100

    feat: added userservice method to check if authenticated user has any of specified roles

--- src/app/header/Header.jsx
+++ src/app/header/Header.jsx
@@ -2,8 +2,11 @@ import { useDispatch, useSelector } from 'react-redux'
 import { NavLink } from 'react-router-dom'
 import { sessionLoginAdminSetAction, sessionLoginUserSetAction, sessionLogoutSetAction } from '../../store/actions/sessionActions'
 
+import UserService from '../../services/UserService'
+import { useKeycloak } from '@react-keycloak/web'
 
 const Header = () => {
+    useKeycloak();
 
     const dispatch = useDispatch()
     const { loggedIn, userType } = useSelector(state => state.sessionReducer)
@@ -34,15 +37,44 @@ const Header = () => {
                         </li>
                     </ul>
                 </nav>
-                {!loggedIn &&
+                {!UserService.getLoggedIn() && (
+                    <button type="button" onClick={() => UserService.login()}>Login</button> 
+                )}
+                {!!UserService.getLoggedIn() && (
+                    <button type="button" onClick={() => UserService.logout()}>Logout</button> 
+                )}
+
+                {!!UserService.getLoggedIn() && (
+                    <p>
+                            isLoggedIn: {UserService.getLoggedIn() ? "true" : "false"}
+                            <br />
+                            getPreferredUsername: {UserService.getPreferredUsername()}
+                            <br />
+                            getEmail: {UserService.getEmail()}
+                            <br />
+                            getEmailVerified: {UserService.getEmailVerified() ? "true" : "false"}
+                            <br />
+                            getName: {UserService.getName()}
+                            <br />
+                            getGivenName: {UserService.getGivenName()}
+                            <br />
+                            getFamilyName: {UserService.getFamilyName()}
+                            <br />
+                            hasRole("user"): {UserService.hasRole(["user"]) ? "true" : "false"}
+                            <br />
+                            hasRole("admin"): {UserService.hasRole(["admin"]) ? "true" : "false"}
+                    </p>
+                )}
+
+                {/*!loggedIn &&
                     <div>
                         <button type="button" onClick={handleBtnLoginUserClick}>Login (as user)</button>
                         <button type="button" onClick={handleBtnLoginAdminClick}>Login (as admin)</button>
                     </div>
-                }
-                {loggedIn &&
+                */}
+                {/*loggedIn &&
                     <button type="button" onClick={handleBtnLogoutClick}>Logout</button>
-                }
+                */}
 
                 <div className='rounded-xl shadow-xl p-5 bg-gradient-to-br from-indigo-500'>
                     TailwindCSS test (Header above ^)
