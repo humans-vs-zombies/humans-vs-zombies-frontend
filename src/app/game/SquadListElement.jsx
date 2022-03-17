0ebc7465128f0f956fb714897171ee7077561d5b
Jørgen <jorgen.saanum@no.experis.com>
2022-03-17 11:34:56 +0100

    REFACTOR: moved squad state to redux

--- src/app/game/SquadListElement.jsx
+++ src/app/game/SquadListElement.jsx
@@ -1,13 +1,22 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { sessionCurrentSquadSetAction } from "../../store/actions/sessionActions";
 
 export const SquadListElement = ({ squad }) => {
+
+  const dispatch = useDispatch()
+
+  const handleBtnJoinSquadClick = (event) => {
+    dispatch(sessionCurrentSquadSetAction(squad))
+  }
+
   return (
     <div className="p-1">
       <div className="flex bg-gray-400 text-center items-center">
         <button className="whitespace-nowrap p-2">{squad.name}</button>
         <div className="sm:block hidden">{ squad.players.filter(p => p.isAlive).length + "/" + squad.players.length}</div>
         <span className="md:block hidden pl-1">Alive</span>
-        <button className="w-16">Join</button>
+        <button className="w-16" onClick={handleBtnJoinSquadClick}>Join</button>
       </div>
     </div>
   );
