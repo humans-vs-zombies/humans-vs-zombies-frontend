be1c9845942863fcf726a752260368786a629e3a
JonasBM <jonas.mela@gmail.com>
2022-03-18 11:06:50 +0100

    FEAT: correct datetime format

--- src/app/editGame/components/GameForm.jsx
+++ src/app/editGame/components/GameForm.jsx
@@ -4,6 +4,8 @@ const GameForm = () => {
 
     const originalGameTitle = "testtitle"
     const originalGameDescription = "Jk"
+    const originalGameDateFrom = "2022-04-01T12:00"
+    const originalGameDateTo = "2022-04-07T12:00"
 
     // Local states
     const [ hasUnsavedChanges, setHasUnsavedChanges ] = useState(false);
@@ -12,11 +14,15 @@ const GameForm = () => {
     const [ game, setGame ] =  useState({
         title: originalGameTitle,
         description: originalGameDescription,
+        dateFrom: originalGameDateFrom,
+        dateTo: originalGameDateTo,
     })
 
     useEffect(() => {
         if (originalGameTitle === game.title &&
-            originalGameDescription === game.description) {
+            originalGameDescription === game.description &&
+            originalGameDateFrom === game.dateFrom &&
+            originalGameDateTo === game.dateTo) {
             setHasUnsavedChanges(false)
             setSumbitBtnBgTW("bg-gray-500")
         }
@@ -49,9 +55,9 @@ const GameForm = () => {
                 </fieldset>
                 <fieldset>
                     <label className="block text-lg text-gray-700 font-bold mb-2" htmlFor="dateFrom">Game date:</label>
-                    <input className="shadow border text-gray-700 rounded ml-4 w-64 pl-10 py-2 px-3" type="datetime-local" id="dateFrom" name="dateFrom" />
+                    <input className="shadow border text-gray-700 rounded ml-4 w-64 pl-10 py-2 px-3" type="datetime-local" id="dateFrom" name="dateFrom" value={ game.dateFrom } onChange={ handleOnInputChange } />
                     <label className="block ml-4 my-1 text-lg" htmlFor="dateTo">to</label>
-                    <input className="shadow border text-gray-700 rounded ml-4 w-64 pl-10 py-2 px-3" type="datetime-local" id="dateTo" name="dateTo" />
+                    <input className="shadow border text-gray-700 rounded ml-4 w-64 pl-10 py-2 px-3" type="datetime-local" id="dateTo" name="dateTo" value={ game.dateTo } onChange={ handleOnInputChange } />
                 </fieldset>
 
                 <button className={`${sumbitBtnBgStyle} ml-4 mt-4 text-white font-bold py-2 px-4 rounded`} type="submit" disabled={ !hasUnsavedChanges }>Save</button>
