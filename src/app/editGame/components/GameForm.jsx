b397c54ae6fab6fc48a0b8ea20aa8aeb763b70c1
JonasBM <jonas.mela@gmail.com>
2022-03-18 09:11:59 +0100

    FEAT: game title input

--- src/app/editGame/components/GameForm.jsx
+++ src/app/editGame/components/GameForm.jsx
@@ -1,17 +1,39 @@
+import React, { useEffect, useState } from "react";
+
 const GameForm = () => {
 
+    const originalGameTitle = "testtitle"
+    
+    // Local states
+    const [ hasUnsavedChanges, setHasUnsavedChanges ] = useState(false);
+    const [ gameTitle, setGameTitle ] = useState(originalGameTitle)
+
+
+    useEffect(() => {
+        if (originalGameTitle === gameTitle) {
+            setHasUnsavedChanges(false)
+        }
+        else {
+            setHasUnsavedChanges(true)
+        }
+    }, [ gameTitle ])
 
     const handleGameTitleChange = ({ target }) => {
+        setGameTitle(target.value)
         console.log(target.value);
     }
 
     return (
         <>
+        <p>fdhsaj</p>
+            <p>jfsdak: {hasUnsavedChanges ? "true" : "false"}</p>
             <form>
                 <fieldset>
                     <label htmlFor="gameTitle">Game title:</label>
-                    <input type="text" id="gameTitle" name="gameTitle" value="John" onChange={ handleGameTitleChange } />
+                    <input type="text" id="gameTitle" name="gameTitle" value={ gameTitle } onChange={ handleGameTitleChange } />
                 </fieldset>
+
+                <button className="Submit-btn Login" type="submit" disabled={ hasUnsavedChanges }>{">"}</button>
             </form>
         </>
     )
