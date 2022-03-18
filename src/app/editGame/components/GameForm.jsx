a8b1b3d5426198c6cdefa1ba03406e2cd894535a
JonasBM <jonas.mela@gmail.com>
2022-03-18 10:55:57 +0100

    FEAT: add datetime input

--- src/app/editGame/components/GameForm.jsx
+++ src/app/editGame/components/GameForm.jsx
@@ -41,13 +41,18 @@ const GameForm = () => {
             <form>
             <fieldset>
                     <label className="block text-lg text-gray-700 font-bold mb-2" htmlFor="title">Game title:</label>
-                    <input className="shadow appearance-none border rounded w-4/5 ml-4 py-2 px-3 text-gray-700 leading-tight focus:outline-none" type="text" id="title" name="title" value={ game.title } onChange={ handleOnInputChange } />
+                    <input className="shadow appearance-none border rounded w-4/5 ml-4 py-2 px-3 text-gray-700 leading-tight" type="text" id="title" name="title" value={ game.title } onChange={ handleOnInputChange } />
                 </fieldset>
                 <fieldset>
                     <label className="block text-lg text-gray-700 font-bold mb-2" htmlFor="description">Game description:</label>
-                    <input className="shadow appearance-none border rounded w-4/5 ml-4 py-2 px-3 text-gray-700 leading-tight focus:outline-none" type="text" id="description" name="description" value={ game.description } onChange={ handleOnInputChange } />
+                    <input className="shadow appearance-none border rounded w-4/5 ml-4 py-2 px-3 text-gray-700 leading-tight" type="text" id="description" name="description" value={ game.description } onChange={ handleOnInputChange } />
                 </fieldset>
-                
+                <fieldset>
+                    <label className="block text-lg text-gray-700 font-bold mb-2" htmlFor="dateFrom">Game date:</label>
+                    <input className="shadow border text-gray-700 rounded ml-4 w-64 pl-10 py-2 px-3" type="datetime-local" id="dateFrom" name="dateFrom" />
+                    <label className="block ml-4 my-1 text-lg" htmlFor="dateTo">to</label>
+                    <input className="shadow border text-gray-700 rounded ml-4 w-64 pl-10 py-2 px-3" type="datetime-local" id="dateTo" name="dateTo" />
+                </fieldset>
 
                 <button className={`${sumbitBtnBgStyle} ml-4 mt-4 text-white font-bold py-2 px-4 rounded`} type="submit" disabled={ !hasUnsavedChanges }>Save</button>
             </form>
