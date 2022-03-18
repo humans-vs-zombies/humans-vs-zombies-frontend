3caba0eae95dc06f4d919c93a127358878948a8d
JonasBM <jonas.mela@gmail.com>
2022-03-18 13:10:26 +0100

    REFACTOR: style constants

--- src/app/editGame/components/GameForm.jsx
+++ src/app/editGame/components/GameForm.jsx
@@ -10,13 +10,20 @@ const GameForm = () => {
     // Local states
     const [ hasUnsavedChanges, setHasUnsavedChanges ] = useState(false);
     const [ sumbitBtnBgStyle, setSumbitBtnBgTW ] = useState("bg-gray-500")
-
     const [ game, setGame ] =  useState({
         title: originalGameTitle,
         description: originalGameDescription,
         dateFrom: originalGameDateFrom,
         dateTo: originalGameDateTo,
     })
+
+    // Style className constants
+    const lableStyle = "block text-lg text-gray-700 font-bold mb-2 mt-6"
+    const inputStyle = "shadow appearance-none border rounded w-4/5 ml-4 py-2 px-3 text-gray-700 leading-tight"
+    const datetimeStyle = "shadow border text-gray-700 rounded ml-4 w-64 pl-10 py-2 px-3"
+    const radioBtnContainerStyle = "inline-block"
+    const radioBtnStyle = "hidden peer"
+    const radioBtnLableStyle = "inline-grid text-gray-700 cursor-pointer appearance-none rounded h-8 w-16 text-center content-center border bg-blue-400 peer-checked:bg-blue-600 focus:outline-none"
 
     useEffect(() => {
         if (originalGameTitle === game.title &&
@@ -46,31 +53,31 @@ const GameForm = () => {
             <p>jfsdak: {hasUnsavedChanges ? "true" : "false"}</p>
             <form>
             <fieldset>
-                    <label className="block text-lg text-gray-700 font-bold mb-2 mt-6" htmlFor="title">Game title:</label>
-                    <input className="shadow appearance-none border rounded w-4/5 ml-4 py-2 px-3 text-gray-700 leading-tight" type="text" id="title" name="title" value={ game.title } onChange={ handleOnInputChange } />
+                    <label className={ lableStyle } htmlFor="title">Game title:</label>
+                    <input className={ inputStyle } type="text" id="title" name="title" value={ game.title } onChange={ handleOnInputChange } />
                 </fieldset>
                 <fieldset>
-                    <label className="block text-lg text-gray-700 font-bold mb-2 mt-6" htmlFor="description">Game description:</label>
-                    <input className="shadow appearance-none border rounded w-4/5 ml-4 py-2 px-3 text-gray-700 leading-tight" type="text" id="description" name="description" value={ game.description } onChange={ handleOnInputChange } />
+                    <label className={ lableStyle } htmlFor="description">Game description:</label>
+                    <input className={ inputStyle } type="text" id="description" name="description" value={ game.description } onChange={ handleOnInputChange } />
                 </fieldset>
                 <fieldset>
-                    <label className="block text-lg text-gray-700 font-bold mb-2 mt-6" htmlFor="dateFrom">Game date:</label>
-                    <input className="shadow border text-gray-700 rounded ml-4 w-64 pl-10 py-2 px-3" type="datetime-local" id="dateFrom" name="dateFrom" value={ game.dateFrom } onChange={ handleOnInputChange } />
+                    <label className={ lableStyle } htmlFor="dateFrom">Game date:</label>
+                    <input className={ datetimeStyle } type="datetime-local" id="dateFrom" name="dateFrom" value={ game.dateFrom } onChange={ handleOnInputChange } />
                     
                     <label className="block ml-4 my-1 text-lg" htmlFor="dateTo">to</label>
-                    <input className="shadow border text-gray-700 rounded ml-4 w-64 pl-10 py-2 px-3" type="datetime-local" id="dateTo" name="dateTo" value={ game.dateTo } onChange={ handleOnInputChange } />
+                    <input className={ datetimeStyle } type="datetime-local" id="dateTo" name="dateTo" value={ game.dateTo } onChange={ handleOnInputChange } />
                 </fieldset>
                 <fieldset>
-                    <label className="block text-lg text-gray-700 font-bold mb-2 mt-6" htmlFor="participants">Game participants:</label>
+                    <label className={ lableStyle } htmlFor="participants">Game participants:</label>
                     
-                    <div className="inline-block">
-                        <input className="hidden peer" type="radio" name="participantsRadioOptions" id="participants10" value="option1" />
-                        <label className="inline-grid text-gray-700 cursor-pointer appearance-none rounded h-8 w-16 text-center content-center border bg-blue-400 peer-checked:bg-blue-600 focus:outline-none" htmlFor="participants10">10</label>
+                    <div className={ radioBtnContainerStyle }>
+                        <input className={ radioBtnStyle } type="radio" name="participantsRadioOptions" id="participants10" value="option1" />
+                        <label className={ radioBtnLableStyle } htmlFor="participants10">10</label>
                     </div>
 
-                    <div className="inline-block">
-                        <input className="hidden peer" type="radio" name="participantsRadioOptions" id="participants25" value="option2" />
-                        <label className="inline-grid text-gray-700 cursor-pointer appearance-none rounded h-8 w-16 text-center content-center border bg-blue-400 peer-checked:bg-blue-600 focus:outline-none" htmlFor="participants25">25</label>
+                    <div className={ radioBtnContainerStyle }>
+                        <input className={ radioBtnStyle } type="radio" name="participantsRadioOptions" id="participants25" value="option2" />
+                        <label className={ radioBtnLableStyle } htmlFor="participants25">25</label>
                     </div>                    
                 </fieldset>
 
