643d301f51e5dd90d1c631583593ee4f7670c09a
JonasBM <jonas.mela@gmail.com>
2022-03-20 16:24:34 +0100

    REFACTOR: remove unnecessary code

--- src/app/home/components/hoc/TdEditGamesTable.jsx
+++ src/app/home/components/hoc/TdEditGamesTable.jsx
@@ -1,15 +1,11 @@
-import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
 const TdEditGamesTable = ({ children }) => {
 
-    const { loggedIn, userType } = useSelector(state => state.sessionReducer)
     const navigate = useNavigate();
 
     const handleEditGameClick = event => {
-        if (loggedIn && userType === "admin") {
             navigate("/game/edit")
-        }
     }
 
 
