f4ac58b6d586202c9daad714b266028866ded92d
JonasBM <jonas.mela@gmail.com>
2022-03-20 17:04:52 +0100

    REFACTRO: remove unused import

--- src/app/game/Game.jsx
+++ src/app/game/Game.jsx
@@ -1,5 +1,4 @@
 import { useDispatch, useSelector } from "react-redux";
-import { Navigate } from "react-router-dom";
 import { GameHeader } from "./GameHeader";
 import { GameMap } from "./GameMap";
 import { BiteCodeInput } from "./BiteCodeInput";
