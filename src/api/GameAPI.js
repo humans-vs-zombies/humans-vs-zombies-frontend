1b5bc71d07dff53d64e0b272a4bdc0f6a7d47927
Jonas Kristoffersen <jo_nas@live.no>
2022-03-21 17:26:53 +0100

    feat: changed API to make requests to remotely hosted heroku, instead of localhost

--- src/api/GameAPI.js
+++ src/api/GameAPI.js
@@ -2,7 +2,7 @@
 import AuthInterceptor from "../utils/AuthInterceptor";
 
 // Switch between localhost and remote host
-const API_URL = "http://localhost:8080"; // "https://humans-vs-zombies-backend.herokuapp.com"
+const API_URL = "https://humans-vs-zombies-backend.herokuapp.com";//"http://localhost:8080"; // "https://humans-vs-zombies-backend.herokuapp.com"
 
 export const GameAPI = {
     getGames() {
@@ -21,7 +21,7 @@ export const GameAPI = {
             "state": "CONFIGURATION",
             "description": "Test game description"
         }
-        
+
         AuthInterceptor.post(API_URL + "/api/v1/game", testGame)
         .then((res) => {
             console.log(res.data);
