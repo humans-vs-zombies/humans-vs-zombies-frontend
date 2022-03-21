42818f36b92a33cb26d061ec3c19583ebf99ec25
Jonas Kristoffersen <jo_nas@live.no>
2022-03-21 16:52:00 +0100

    refactor: renamed UserService to KeycloakService

--- src/services/KeycloakService.js
+++ src/services/KeycloakService.js
@@ -6,6 +6,20 @@ const _kc = new Keycloak({
     clientId: "humans-vs-zombies-frontend"
 });
 
+const initKeycloak = (onAuthenticatedCallback) => {
+    _kc.init({
+        onLoad: 'check-sso',
+        silentCheckSsoRedirectUri: window.location.origin + '/silent-check-sso.html',
+        pkceMethod: 'S256'
+    })
+    .then(() => {
+        onAuthenticatedCallback();
+    })
+    .catch((error) => {
+        console.error("Failed to initialize keycloak: " + error);
+    })
+};
+
 // Fields in _kc.tokenParsed  {exp, iat, auth_time, jti, iss, aud, sub, typ, azp, nonce, session_state, acr, allowed-origins, realm_access, resource_access, scope, sid, email_verified, name, preferred_username, given_name, family_name, email})
 
 const login = _kc.login;
@@ -21,10 +35,11 @@ const getContext = () => _kc.getContext();
 const hasRole = (roles) => roles.some((role) => _kc.hasResourceRole(role));
 const getToken = () => _kc.token;
 
-const UserService = {
+const KeycloakService = {
     _kc,
     login,
     logout,
+    initKeycloak,
     getLoggedIn,
     getPreferredUsername,
     getEmail,
@@ -37,4 +52,4 @@ const UserService = {
     getToken
 };
 
-export default UserService;
+export default KeycloakService;
