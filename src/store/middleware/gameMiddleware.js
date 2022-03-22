78258a194ebbd7e02766af5b8105e35e7df08302
JonasBM <jonas.mela@gmail.com>
2022-03-22 15:41:49 +0100

    FEAT: delete game api

--- src/store/middleware/gameMiddleware.js
+++ src/store/middleware/gameMiddleware.js
@@ -1,5 +1,5 @@
 import { GameAPI } from "../../api/GameAPI";
-import { ACTION_GAMES_GET_ATTEMPTING, gamesGetErrorAction, gamesGetSuccessAction } from "../actions/gameActions";
+import { ACTION_GAMES_DELETE_ATTEMPTING, ACTION_GAMES_GET_ATTEMPTING, gamesGetErrorAction, gamesGetSuccessAction } from "../actions/gameActions";
 
 export const gameMiddleware = ({ dispatch }) => next => action => {
 
@@ -7,7 +7,7 @@ export const gameMiddleware = ({ dispatch }) => next => action => {
 
     switch (action.type) {
         case ACTION_GAMES_GET_ATTEMPTING:
-            // Try to get games
+            // Attempt to get games
             GameAPI.getGames()
             .then(res => {
                 dispatch(gamesGetSuccessAction(res.data.payload))
@@ -15,6 +15,11 @@ export const gameMiddleware = ({ dispatch }) => next => action => {
             .catch((error) => {
                 dispatch(gamesGetErrorAction("Unable to fetch games (" + error.message + ")"))
             });
+            break
+        
+        case ACTION_GAMES_DELETE_ATTEMPTING:
+            // Attempt to delete a game
+            
             break
 
         default:
