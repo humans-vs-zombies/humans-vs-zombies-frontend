f6503dffb500dc3ccb3c6ad2837a937ac35fa253
JonasBM <jonas.mela@gmail.com>
2022-03-23 13:53:09 +0100

    CHORE: create game actions

--- src/store/actions/gameActions.js
+++ src/store/actions/gameActions.js
@@ -1,10 +1,17 @@
 export const ACTION_GAMES_GET_ATTEMPTING = "[getGames] ATTEMPT"
 export const ACTION_GAMES_GET_SUCCESS = "[getGames] SUCCESS"
 export const ACTION_GAMES_GET_ERROR = "[getGames] ERROR"
+
+export const ACTION_GAME_CREATE_ATTEMPTING = "[createGame] ATTEMPT"
+export const ACTION_GAME_CREATE_SUCCESS = "[createGame] SUCCESS"
+export const ACTION_GAME_CREATE_ERROR = "[createGame] ERROR"
+
 export const ACTION_GAMES_DELETE_ATTEMPTING = "[deleteGames] ATTEMPT"
 export const ACTION_GAMES_DELETE_SUCCESS = "[deleteGames] SUCCESS"
 export const ACTION_GAMES_DELETE_ERROR = "[deleteGames] ERROR"
 
+
+// Get
 export const gamesGetAttemptAction = (limit, offset) => ({
     type: ACTION_GAMES_GET_ATTEMPTING,
     payload: limit, offset
@@ -20,6 +27,22 @@ export const gamesGetErrorAction = error => ({
     payload: error
 })
 
+// Create
+export const gameCreateAttemptAction = (title, description, dateFrom, dateTo, participants) => ({
+    type: ACTION_GAME_CREATE_ATTEMPTING,
+    payload: title, description, dateFrom, dateTo, participants
+})
+
+export const gameCreateSuccessAction = () => ({
+    type: ACTION_GAME_CREATE_SUCCESS,
+})
+
+export const gameCreateErrorAction = error => ({
+    type: ACTION_GAME_CREATE_ERROR,
+    payload: error
+})
+
+// Delete
 export const gamesDeleteAttemptAction = id => ({
     type: ACTION_GAMES_DELETE_ATTEMPTING,
     payload: id,
