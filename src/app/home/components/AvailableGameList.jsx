74a1f3ad0865eae94ef1b64377aaf017655a9bb0
JonasBM <jonas.mela@gmail.com>
2022-03-26 13:47:28 +0100

    FEAT: radio btn instead of btn's

--- src/app/home/components/AvailableGameList.jsx
+++ src/app/home/components/AvailableGameList.jsx
@@ -11,6 +11,12 @@ const AvailableGameList = () => {
     const loggedIn = KeycloakService.getLoggedIn()
     const hasAdminRole = KeycloakService.hasRole(["admin"])
     const { gamesGetAttempting, gamesGetSuccess, gamesGetError, games } = useSelector(state => state.gameReducer)
+
+    // Style className constants
+    const lableStyle = "block text-lg text-gray-700 font-bold mb-2 mt-6"
+    const radioBtnContainerStyle = "inline-block"
+    const radioBtnStyle = "hidden peer"
+    const radioBtnLableStyle = "inline-grid bg-blue-500 hover:bg-blue-700 ml-4 my-4 text-white font-bold py-2 px-4 rounded peer-checked:bg-blue-800 focus:outline-none"
     const filterBtnStyle = "bg-blue-500 hover:bg-blue-700 ml-4 my-4 text-white font-bold py-2 px-4 rounded"
     
     useEffect(() => {
@@ -47,11 +53,33 @@ const AvailableGameList = () => {
         <>
             <main>
                 <h2>Available games</h2>
-                <button className={ filterBtnStyle } value="ALL" onClick={ handleOnBtnClickFilterGames }>All</button>
-                <button className={ filterBtnStyle } value="CONFIGURATION" onClick={ handleOnBtnClickFilterGames }>Configuration</button>
-                <button className={ filterBtnStyle } value="REGISTRATION" onClick={ handleOnBtnClickFilterGames }>Registration</button>
-                <button className={ filterBtnStyle } value="IN_PROGRESS" onClick={ handleOnBtnClickFilterGames }>In progress</button>
-                <button className={ filterBtnStyle } value="COMPLETE" onClick={ handleOnBtnClickFilterGames }>Complete</button>
+                <fieldset>
+                    <label className={ lableStyle } htmlFor="participants">Filter:</label>
+                    <div className={ radioBtnContainerStyle }>
+                        <input className={ radioBtnStyle } type="radio" name="filterRadioOptions" id="all" value="ALL" onChange={ handleOnBtnClickFilterGames } defaultChecked />
+                        <label className={ radioBtnLableStyle } htmlFor="all">All</label>
+                    </div>
+
+                    <div className={ radioBtnContainerStyle }>
+                        <input className={ radioBtnStyle } type="radio" name="filterRadioOptions" id="configuration" value="CONFIGURATION" onChange={ handleOnBtnClickFilterGames } />
+                        <label className={ radioBtnLableStyle } htmlFor="configuration">Configuration</label>
+                    </div>
+
+                    <div className={ radioBtnContainerStyle }>
+                        <input className={ radioBtnStyle } type="radio" name="filterRadioOptions" id="registration" value="REGISTRATION" onChange={ handleOnBtnClickFilterGames } />
+                        <label className={ radioBtnLableStyle } htmlFor="registration">Registration</label>
+                    </div>
+
+                    <div className={ radioBtnContainerStyle }>
+                        <input className={ radioBtnStyle } type="radio" name="filterRadioOptions" id="in_progress" value="IN_PROGRESS" onChange={ handleOnBtnClickFilterGames } />
+                        <label className={ radioBtnLableStyle } htmlFor="in_progress">In progress</label>
+                    </div>
+
+                    <div className={ radioBtnContainerStyle }>
+                        <input className={ radioBtnStyle } type="radio" name="filterRadioOptions" id="complete" value="COMPLETE" onChange={ handleOnBtnClickFilterGames } />
+                        <label className={ radioBtnLableStyle } htmlFor="complete">Complete</label>
+                    </div>
+                </fieldset>
                 <table className="border-collapse border-4 min-w-full">
                     <thead className="bg-gray-100">
                         <tr className="border-2">
