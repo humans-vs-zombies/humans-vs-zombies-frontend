1d3cdff9e723d8f45fe753d5ca0849c4c7481b9d
JonasBM <jonas.mela@gmail.com>
2022-03-26 11:46:22 +0100

    FEAT: add filter btn's

--- src/app/home/components/AvailableGameList.jsx
+++ src/app/home/components/AvailableGameList.jsx
@@ -36,11 +36,17 @@ const AvailableGameList = () => {
             </>
         )
     }
+    
 
     return (
         <>
             <main>
                 <h2>Available games</h2>
+                <button className="bg-blue-500 hover:bg-blue-700 ml-4 my-4 text-white font-bold py-2 px-4 rounded">All</button>
+                <button className="bg-blue-500 hover:bg-blue-700 ml-4 my-4 text-white font-bold py-2 px-4 rounded">Configuration</button>
+                <button className="bg-blue-500 hover:bg-blue-700 ml-4 my-4 text-white font-bold py-2 px-4 rounded">Registration</button>
+                <button className="bg-blue-500 hover:bg-blue-700 ml-4 my-4 text-white font-bold py-2 px-4 rounded">In progress</button>
+                <button className="bg-blue-500 hover:bg-blue-700 ml-4 my-4 text-white font-bold py-2 px-4 rounded">Complete</button>
                 <table className="border-collapse border-4 min-w-full">
                     <thead className="bg-gray-100">
                         <tr className="border-2">
