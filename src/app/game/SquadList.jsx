9d0627791fbfd199f645303f92086a651f532211
Jørgen <jorgen.saanum@no.experis.com>
2022-03-27 16:22:48 +0200

    FIX: added key to SquadListElement

--- src/app/game/SquadList.jsx
+++ src/app/game/SquadList.jsx
@@ -5,7 +5,7 @@ export const SquadList = ({ squads }) => {
   return (
     <>
       {squads.map((squad) => (
-        <SquadListElement squad={squad} />
+        <SquadListElement squad={squad} key={squad.id} />
       ))}
     </>
   );
