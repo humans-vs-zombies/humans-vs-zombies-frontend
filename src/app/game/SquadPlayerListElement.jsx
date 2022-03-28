f197fe6817eee2ed848584d5591a1e2beb1301bb
Jorgsaa <jorgen.saanum@gmail.com>
2022-03-28 14:17:09 +0200

    STYLE: text and background colors

--- src/app/game/SquadPlayerListElement.jsx
+++ src/app/game/SquadPlayerListElement.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 export const SquadPlayerListElement = ({ player }) => {
   return (
-    <div className="flex px-2 bg-gray-400 rounded m-2">
+    <div className="flex px-2 bg-blue-500 hover:bg-blue-600 rounded m-2 text-center text-white">
       <div className="p-2">{player.name}</div>
-      <div className="p-2">Alive: {player.human ? "Yes" : "No"}</div>
+      <div className="p-2 flex-grow text-right">Alive: {player.human ? "Yes" : "No"}</div>
     </div>
   );
 };
