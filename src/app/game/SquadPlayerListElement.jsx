2913892678b187138d35174a33ef353dc92d7ca8
Jorgsaa <jorgen.saanum@gmail.com>
2022-03-28 13:11:13 +0200

    FIX: isAlive -> human

--- src/app/game/SquadPlayerListElement.jsx
+++ src/app/game/SquadPlayerListElement.jsx
@@ -4,7 +4,7 @@ export const SquadPlayerListElement = ({ player }) => {
   return (
     <div className="flex px-2 bg-gray-400 rounded m-2">
       <div className="p-2">{player.name}</div>
-      <div className="p-2">Alive: {player.isAlive ? "Yes" : "No"}</div>
+      <div className="p-2">Alive: {player.human ? "Yes" : "No"}</div>
     </div>
   );
 };
