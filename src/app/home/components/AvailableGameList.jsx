140eb3bc1d8bb78f0d9d7b1a3d8d7bac7e4651c3
JonasBM <jonas.mela@gmail.com>
2022-03-28 10:31:09 +0200

    STYLE: filter responsive

--- src/app/home/components/AvailableGameList.jsx
+++ src/app/home/components/AvailableGameList.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import KeycloakService from "../../../services/KeycloakService"
 import { gameInitAction, gamesGetAttemptAction } from "../../../store/actions/gameActions"
+import CenterChild from "../../commonComponents/hoc/CenterChild"
 import AvailableGame from "./AvailableGame"
 import TdMessageGamesTable from "./hoc/TdMessageGamesTable"
 import ThGamesTable from "./hoc/ThGamesTable"
@@ -18,7 +19,7 @@ const AvailableGameList = () => {
     // Style className constants
     const radioBtnContainerStyle = "inline-block"
     const radioBtnStyle = "hidden peer"
-    const radioBtnLableStyle = "inline-grid bg-blue-500 hover:bg-blue-700 ml-4 my-1 text-white font-bold py-2 px-4 rounded peer-checked:bg-blue-800 focus:outline-none"
+    const radioBtnLableStyle = "inline-grid bg-blue-500 hover:bg-blue-700 ml-4 my-1 text-white font-bold py-2 px-4 rounded peer-checked:bg-blue-800 focus:outline-none w-32 text-center sm:w-fit"
     let rowGridCols = hasAdminRole ? "lg:grid-cols-[auto,_270px,_120px,_120px,_80px,_80px]" : "lg:grid-cols-[auto,_270px,_120px,_120px]"
     
     useEffect(() => {
@@ -75,35 +76,37 @@ const AvailableGameList = () => {
     return (
         <>
             <main>
-                <fieldset>
-                    <label className="block text-lg mb-2 lg:mt-10" htmlFor="participants">Filter:</label>
-                    <div className={ radioBtnContainerStyle }>
-                        <input className={ radioBtnStyle } type="radio" name="filterRadioOptions" id="all" value="ALL" onChange={ handleOnBtnClickFilterGames } defaultChecked />
-                        <label className={ radioBtnLableStyle } htmlFor="all">All</label>
-                    </div>
+                <CenterChild>
+                    <fieldset className="grid grid-cols-[auto,_1fr] sm:block">
+                        <label className="block text-lg self-center sm:mb-2 lg:mt-10" htmlFor="participants">Filter:</label>
+                        <div className={ radioBtnContainerStyle }>
+                            <input className={ radioBtnStyle } type="radio" name="filterRadioOptions" id="all" value="ALL" onChange={ handleOnBtnClickFilterGames } defaultChecked />
+                            <label className={ radioBtnLableStyle } htmlFor="all">All</label>
+                        </div>
 
-                    { hasAdminRole &&
-                    <div className={ radioBtnContainerStyle }>
-                        <input className={ radioBtnStyle } type="radio" name="filterRadioOptions" id="configuration" value="CONFIGURATION" onChange={ handleOnBtnClickFilterGames } />
-                        <label className={ radioBtnLableStyle } htmlFor="configuration">Configuration</label>
-                    </div>
-                    }
+                        { hasAdminRole &&
+                        <div className={ radioBtnContainerStyle }>
+                            <input className={ radioBtnStyle } type="radio" name="filterRadioOptions" id="configuration" value="CONFIGURATION" onChange={ handleOnBtnClickFilterGames } />
+                            <label className={ radioBtnLableStyle } htmlFor="configuration">Configuration</label>
+                        </div>
+                        }
 
-                    <div className={ radioBtnContainerStyle }>
-                        <input className={ radioBtnStyle } type="radio" name="filterRadioOptions" id="registration" value="REGISTRATION" onChange={ handleOnBtnClickFilterGames } />
-                        <label className={ radioBtnLableStyle } htmlFor="registration">Registration</label>
-                    </div>
+                        <div className={ radioBtnContainerStyle }>
+                            <input className={ radioBtnStyle } type="radio" name="filterRadioOptions" id="registration" value="REGISTRATION" onChange={ handleOnBtnClickFilterGames } />
+                            <label className={ radioBtnLableStyle } htmlFor="registration">Registration</label>
+                        </div>
 
-                    <div className={ radioBtnContainerStyle }>
-                        <input className={ radioBtnStyle } type="radio" name="filterRadioOptions" id="in_progress" value="IN_PROGRESS" onChange={ handleOnBtnClickFilterGames } />
-                        <label className={ radioBtnLableStyle } htmlFor="in_progress">In progress</label>
-                    </div>
+                        <div className={ radioBtnContainerStyle }>
+                            <input className={ radioBtnStyle } type="radio" name="filterRadioOptions" id="in_progress" value="IN_PROGRESS" onChange={ handleOnBtnClickFilterGames } />
+                            <label className={ radioBtnLableStyle } htmlFor="in_progress">In progress</label>
+                        </div>
 
-                    <div className={ radioBtnContainerStyle }>
-                        <input className={ radioBtnStyle } type="radio" name="filterRadioOptions" id="complete" value="COMPLETE" onChange={ handleOnBtnClickFilterGames } />
-                        <label className={ radioBtnLableStyle } htmlFor="complete">Complete</label>
-                    </div>
-                </fieldset>
+                        <div className={ radioBtnContainerStyle }>
+                            <input className={ radioBtnStyle } type="radio" name="filterRadioOptions" id="complete" value="COMPLETE" onChange={ handleOnBtnClickFilterGames } />
+                            <label className={ radioBtnLableStyle } htmlFor="complete">Complete</label>
+                        </div>
+                    </fieldset>
+                </CenterChild>
                 <table className="border-collapse border-4 mt-3 min-w-full grid grid-cols-[auto,_1fr] lg:block">
                     <thead className="bg-gray-100 grid w-full">
                         <tr className={`border-2 grid ${rowGridCols} px-4 lg:gap-4`}>
