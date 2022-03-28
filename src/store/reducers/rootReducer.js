1097a1c76f4986d0c9162492f203102c27c8729d
Jorgsaa <jorgen.saanum@gmail.com>
2022-03-28 13:01:01 +0200

    FEAT: add squadMiddleware and squadReducer

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -1,11 +1,12 @@
 import { combineReducers } from "redux";
-import { sessionReducer } from "./sessionReducer"
+import { sessionReducer } from "./sessionReducer";
 import { gameReducer } from "./gameReducer";
-
+import { squadReducer } from "./squadReducer";
 
 const appReducer = combineReducers({
-    sessionReducer,
-    gameReducer
-})
+  sessionReducer,
+  gameReducer,
+  squadReducer,
+});
 
 export default appReducer;
