50a71a66e8d139d9c507f62e10c052b086cb2af0
Jorgsaa <jorgen.saanum@gmail.com>
2022-03-28 12:57:47 +0200

    FIX: squad endpoint root

--- src/api/SquadAPI.js
+++ src/api/SquadAPI.js
@@ -2,7 +2,7 @@ import AuthInterceptor from "../utils/AuthInterceptor";
 
 const API_URL = "https://humans-vs-zombies-backend.herokuapp.com";
 
-const ROOT_URI = (gameId) => API_URL + "/api/v1/game/" + gameId + "/squads"
+const ROOT_URI = (gameId) => API_URL + `/api/v1/game/${gameId}/squad`
 
 export const SquadAPI = {
 
