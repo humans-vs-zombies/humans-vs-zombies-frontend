182dd0c95ab2abd754c6c940d5fbea393d6b9799
Jonas <50987975+Pucko321@users.noreply.github.com>
2022-03-29 19:58:38 +0200

    FEAT: show username not given name

--- src/app/header/Header.jsx
+++ src/app/header/Header.jsx
@@ -13,7 +13,7 @@ const Header = () => {
                 }
                 { KeycloakService.getLoggedIn() &&
                 <>
-                    <h2 className='col-start-1 row-start-1 justify-self-center self-end text-lg hidden sm:block md:text-2xl'>Welcome {KeycloakService.getGivenName()}</h2>
+                    <h2 className='col-start-1 row-start-1 justify-self-center self-end text-lg hidden sm:block md:text-2xl'>Welcome {KeycloakService.getPreferredUsername()}</h2>
                     <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded max-w-fit justify-self-center col-start-3 row-start-1 justify-self-center self-end text-sm md:text-lg" type="button" onClick={() => KeycloakService.logout()}>Logout</button>
                 </> 
                 }
